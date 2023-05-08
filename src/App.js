7877dc67d832b924faabb2f5033264447c016cb7
Jon Chen <jchen2190@gmail.com>
2023-05-07 23:52:27 -0400

    add svgs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import NavHeader from './components/NavHeader';
-import NavSidebar from './components/NavSidebar'
 import Home from './Home';
 import Tasklist from "./components/Tasklist";
 import OneTask from './components/OneTask';
@@ -15,20 +14,16 @@ function App() {
     return (
         <div className="app">
             <NavHeader />
-            <div className="content-container">
-                <NavSidebar />
-                <div className="main-content">
-                    <Routes>
-                        <Route path="/" element={ <Home /> } />
-                        <Route path="/tasklist" element={ <Tasklist /> } />
-                        <Route path="/tasklist/:id" element={ <OneTask /> } />
-                        <Route path="/login" element={ <LogIn /> } />
-                        <Route path="/signup" element = { <SignUp /> } />
-                        <Route path="/profile" element={ <Profile /> } />
-                        <Route path="*" element={ <NotFound /> } />
-                    </Routes>
-                </div>
-            </div>
+
+            <Routes>
+                <Route path="/" element={ <Home /> } />
+                <Route path="/tasklist" element={ <Tasklist /> } />
+                <Route path="/tasklist/:id" element={ <OneTask /> } />
+                <Route path="/login" element={ <LogIn /> } />
+                <Route path="/signup" element = { <SignUp /> } />
+                <Route path="/profile" element={ <Profile /> } />
+                <Route path="*" element={ <NotFound /> } />
+            </Routes>
         </div>
     );
 }
