61bd9b731ca3ec684cd0da72c294dadb15bfe03f
Jon Chen <jchen2190@gmail.com>
2023-05-09 00:55:10 -0400

    style add task form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { API_URL } from './constants';
 
 function AddTask() {
-    const [status, setStatus] = useState("open");
+    // const [status, setStatus] = useState("open");
     const [subject, setSubject] = useState("");
     const [author, setAuthor] = useState("guest");
     const [description, setDescription] = useState("");
@@ -41,7 +41,7 @@ function AddTask() {
     async function postTask() { 
 
         let newTask = {
-            status: status,
+            status: "open",
             created: Date.now(),
             subject: subject,
             author: author,
@@ -72,49 +72,72 @@ function AddTask() {
     }
 
     return (
-        <div className="d-flex container align-items-center">
-            <button type="button" className="btn btn-primary mb-3 justify-content-center rounded-pill" onClick={toggleEditing}>
-                { isEditing ? "- Cancel New Issue" : "+ Add New Issue" }
-            </button>
+        <div className="d-flex container align-items-center row">
+            <div>
+                <button type="button" className="btn btn-primary mb-3 justify-content-center rounded-pill" onClick={toggleEditing}>
+                    { isEditing ? "- Cancel New Issue" : "+ Add New Issue" }
+                </button>
+            </div>
             {isEditing ? 
-                <form onSubmit = {handleSubmit} className="addTaskForm rounded p-3">
-                    <div className="form-outline mb-2">
-                        <label className="form-label">Status</label>
-                        <select value={status} onChange={(e) => setStatus(e.target.value)}>
-                            <option value="open">Open</option>
-                            <option value="closed">Closed</option>
-                        </select>
+                <form onSubmit = {handleSubmit} className="addTaskForm container rounded p-3">
+                    <div>
+                        {/* <div className="mb-3 row">
+                            <label className="form-label">Status</label>
+                            <select value={status} onChange={(e) => setStatus(e.target.value)}>
+                                <option value="open">Open</option>
+                                <option value="closed">Closed</option>
+                            </select>
+                        </div> */}
+                        <div className="mb-3 row">
+                            <label className="col-sm-2 col-form-label">Summary*</label>
+                            <div className="col-sm-10">
+                                <input className="form-control" value={subject} required onChange={(e) => setSubject(e.target.value)}/>
+                            </div>
+                        </div>
+                        <div className="mb-3 row">
+                            <label className="col-sm-2 col-form-label">Description</label>
+                            <div className="col-sm-10">
+                                <textarea
+                                    className="form-control"
+                                    value={description}
+                                    required
+                                    onChange={(e) => setDescription(e.target.value)} />
+                            </div>
+                        </div>
+                        <div className="mb-3 row">
+                            <label className="col-sm-2 col-form-label">Assigned To</label>
+                            <div className="col-sm-4">
+                                <input className="form-control"
+                                    value={assignTo}
+                                    required
+                                    onChange={(e) => setAssignTo(e.target.value)} />
+                            </div>
+                            <button type="button" className="btn btn-primary col-3" onClick={() => setAssignTo(author)}>Assign to myself</button>
+                        </div>
+                        <div className="mb-3 row">
+                            <label className="col-sm-2 col-form-label">Due Date:</label>
+                            <div className="col-sm-4">
+                                <input
+                                    className="form-control"
+                                    value={dueDate.toISOString().slice(0, 10)}
+                                    type="date"
+                                    placeholder="Date Due"
+                                    autoComplete="off"
+                                    onChange={(e) => setDueDate(new Date(e.target.value))} />
+                            </div>
+                        </div>
+                        <div className="mb-3 row">
+                            <label className="col-sm-2 col-form-label">Priority</label>
+                            <div className="col-sm-3">
+                                <select className="form-select" value={importance} required onChange={(e) => setImportance(e.target.value)}>
+                                    <option value="low">Low</option>
+                                    <option value="medium">Medium</option>
+                                    <option value="high">High</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </div>
-                    <div className="form-outline mb-2">
-                        <label className="form-label">Subject</label>
-                        <input value={subject} required onChange={(e) => setSubject(e.target.value)}/>
-                    </div>
-                    <div className="form-outline mb-2">
-                        <label className="form-label">Description</label>
-                        <input value={description} required onChange={(e) => setDescription(e.target.value)}/>
-                    </div>
-                    <div className="form-outline mb-2">
-                        <label className="form-label">Assigned To</label>
-                        <input value={assignTo} required onChange={(e) => setAssignTo(e.target.value)} />
-                    </div>
-                    <div className="form-outline mb-2">
-                        <label className="form-label">Due Date:</label>
-                        <input
-                            value={dueDate.toISOString().slice(0, 10)}
-                            type="date"
-                            placeholder="Date Due"
-                            autoComplete="off"
-                            onChange={(e) => setDueDate(new Date(e.target.value))} />
-                    </div>
-                    <div className="form-outline mb-2">
-                        <label className="form-label">Priority</label>
-                        <select value={importance} required onChange={(e) => setImportance(e.target.value)}>
-                            <option value="low">Low</option>
-                            <option value="medium">Medium</option>
-                            <option value="high">High</option>
-                        </select>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
                 : <></>
             }
